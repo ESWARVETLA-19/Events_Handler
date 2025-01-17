66dd03c1b5ae6195fb2949eb3137173ab1fd7ff9
ESWARA RAO VETLA <112643496+ESWARVETLA-19@users.noreply.github.com>
2025-01-17 11:36:50 +0000

    added

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"rest_api/project/models"
+	"github.com/gin-gonic/gin"
+)
+func main(){
+	server:=gin.Default()
+	server.GET("/events",getEvenets)
+	server.POST("/events",createEvent)
+	server.Run(":8080")
+}
+
+func getEvenets(context *gin.Context){
+	events:=models.GetAllEvents()
+	context.JSON(http.StatusOK,events)//gin.H{"message":"hello"}
+}
+
+func createEvent(context *gin.Context){
+	var event models.Event
+	err:=context.ShouldBindJSON(&event)//work like scan 
+
+	if err!=nil{
+		context.JSON(http.StatusBadRequest,gin.H{"message":"could not parse data"})
+	}
+
+	event.Id=1
+	event.UserId=1
+	context.JSON(http.StatusCreated, gin.H{"message": "created event!","event":event})
+
+}
